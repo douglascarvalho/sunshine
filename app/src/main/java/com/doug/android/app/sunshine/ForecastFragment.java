de08dc4b1757f9a0eaa55b34fc51a410d1b0861c
doug <douglaspdc@gmail.com>
2016-10-19 10:08:06 -0200

    launch settings activity

--- app/src/main/java/com/doug/android/app/sunshine/ForecastFragment.java
+++ app/src/main/java/com/doug/android/app/sunshine/ForecastFragment.java
@@ -65,6 +65,7 @@ public class ForecastFragment extends Fragment {
             weatherTask.execute("94043");
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -80,11 +81,7 @@ public class ForecastFragment extends Fragment {
                 "Thurs 6/26 - Rainy - 18/11",
                 "Fri 6/27 - Foggy - 21/10",
                 "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7",
-                "Sun 6/29 - Sunny - 20/7 TESTE",
-                "Sun 6/29 - Sunny - 20/7 TESTE 2",
-                "Sun 6/29 - Sunny - 20/7 TESTE 3",
-                "Sun 6/29 - Sunny - 20/7 TESTE 4"
+                "Sun 6/29 - Sunny - 20/7"
 
         };
         List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
--- app/src/main/java/com/doug/android/app/sunshine/MainActivity.java
+++ app/src/main/java/com/doug/android/app/sunshine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.doug.android.app.sunshine;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -51,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
