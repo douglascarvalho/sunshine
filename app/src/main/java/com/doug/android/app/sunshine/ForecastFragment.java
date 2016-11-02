54ed53b1c3f00883f2e5f3728e3bad73f02cf994
doug <douglaspdc@gmail.com>
2016-11-02 11:42:06 -0200

    setting alarm

--- app/src/main/java/com/doug/android/app/sunshine/ForecastFragment.java
+++ app/src/main/java/com/doug/android/app/sunshine/ForecastFragment.java
@@ -1,5 +1,8 @@
 package com.doug.android.app.sunshine;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -100,9 +103,17 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        Intent intent = new Intent(getActivity(), SunshineService.class);
+        Intent intent = new Intent(getActivity(), SunshineService.AlarmReceiver.class);
         intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA, Utility.getPreferredLocation(getActivity()));
-        getActivity().startService(intent);
+
+        AlarmManager alarmManager = (AlarmManager) getActivity().getSystemService(Context.ALARM_SERVICE);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(getActivity(), 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+        alarmManager.set(
+                AlarmManager.RTC_WAKEUP,
+                System.currentTimeMillis() + 5000,
+                alarmIntent
+                );
     }
 
     public void setmUseTodayLayout(boolean mUseTodayLayout) {
