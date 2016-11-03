ca7741ac90fd24b5c63a18d428b3ca9c33e43374
doug <douglaspdc@gmail.com>
2016-11-03 15:04:11 -0200

    notification

--- app/src/main/java/com/doug/android/app/sunshine/Utility.java
+++ app/src/main/java/com/doug/android/app/sunshine/Utility.java
@@ -30,6 +30,18 @@ public class Utility {
     // back into date objects for comparison/processing.
     public static final String DATE_FORMAT = "yyyyMMdd";
 
+    public static String formatTemperature(Context context, double temperature) {
+        // Data stored in Celsius by default.  If user prefers to see in Fahrenheit, convert
+        // the values here.
+        String suffix = "\u00B0";
+        if (!isMetric(context)) {
+            temperature = (temperature * 1.8) + 32;
+        }
+
+        // For presentation, assume the user doesn't care about tenths of a degree.
+        return String.format(context.getString(R.string.format_temperature), temperature);
+    }
+
     public static String getPreferredLocation(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_location_key), context.getString(R.string.pref_location_default));
